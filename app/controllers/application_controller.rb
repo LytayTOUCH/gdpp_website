455494a3686c3d351cdce3fdbfbf8af84b938b69
lytay <lytaytouch@gmail.com>
2015-10-26 10:42:34 +0700

    Update notifcationa and modify path after signout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,7 +13,7 @@ class ApplicationController < ActionController::Base
   private
 
   def after_sign_out_path_for(resource_or_scope)
-    root_path
+    new_admin_session_path
   end
 
   def after_sign_in_path_for(resource_or_scope)
