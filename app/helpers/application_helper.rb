1609bbbb002873294f7189d065569e3b5da27bf9
lytay <lytaytouch@gmail.com>
2015-10-26 15:14:00 +0700

    Add protonote js for setting up comments note on webpage

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def gmap_lib
+    javascript_include_tag 'https://maps.googleapis.com/maps/api/js?sensor=false'
+  end
+
+  def ingetrate_protnote
+    javascript_include_tag 'http://www.protonotes.com/js/protonotes.js'
+  end
+
 end
